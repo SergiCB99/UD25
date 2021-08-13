2903ca8101e58a44516f31ef75d71f32d70c597a
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-08-13 13:01:30 +0200

    Create data.sql

---
+++ UD25.Rest/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+DROP table IF EXISTS empleados;
+
+create table empleados (
+id int(5) auto_increment primary key,
+nombre varchar(100),
+trabajo varchar(100),
+salario int(5)
+);
+
+insert into empleados (nombre,trabajo,salario) values ('Sergi','Junior',1000);
+insert into empleados (nombre,trabajo,salario) values ('Adrian','Junior',1000);
+insert into empleados (nombre,trabajo,salario) values ('Jose','Senior',3000);
+insert into empleados (nombre,trabajo,salario) values ('Luis','Mid',2000);
+insert into empleados (nombre,trabajo,salario) values ('Antonio','Mid',2000);
